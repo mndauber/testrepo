3d872abf01171736e0f541b1e1d0567ae82c9f54
Matthew Dauber <daubdragon@gmail.com>
2016-06-06 17:40:53 -0500

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,17 +1,16 @@
 
 
-cat  <<EOF
+cat  << EOF
 <html>
 <body>
 <h3>An example artifact</h3>
-<pre>
+
 ==== ==== ====
 An example artifact, created on: $(date)
 
 Pipeline which created it: $GO_PIPELINE_NAME
 Pipeline counter was: $GO_PIPELINE_COUNTER
 ==== ==== ====
-</pre>
 </body>
 </html>
 EOF
